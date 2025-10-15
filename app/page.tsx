e0f634f1fab79dd6cf4b465ab4df04f91032bf01
nikhilesh2010 <nikhilesh.cc@gmail.com>
2025-10-16 01:06:12 +0530

    Add theme provider and toggle components; enhance global styles for theme support

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-background text-foreground">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="bg-[--background]">Nikhilesh C</div>
+        <div className="p-4 rounded-lg">Nikhilesh C</div>
+        <ThemeToggle />
         <Image
           className="dark:invert"
           src="/next.svg"
